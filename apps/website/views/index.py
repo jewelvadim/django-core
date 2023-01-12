0989bb84644eaa09c94059e4a6fbcc9edb63b2ca
Vadim <jewel.vadim@gmail.com>
2023-01-12 21:21:53 +0400

    remove print

--- apps/website/views/index.py
+++ apps/website/views/index.py
@@ -10,8 +10,6 @@ class IndexTemplateView(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs: Unpack) -> dict[str, Any]:
-        print(kwargs, type(kwargs))
-
         context = super().get_context_data(**kwargs)
 
         website = Website.get_solo()
