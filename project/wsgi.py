a65c804a3eb483527d6f4017eb61b2350d46611c
Vadim <jewel.vadim@gmail.com>
2023-01-16 20:09:01 +0400

    fix error settings module name

--- project/wsgi.py
+++ project/wsgi.py
@@ -3,6 +3,6 @@ from os import environ
 from django.core.wsgi import get_wsgi_application
 
 
-environ.setdefault('DJANGO_SETTINGS_MODULE', 't.settings')
+environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = get_wsgi_application()
