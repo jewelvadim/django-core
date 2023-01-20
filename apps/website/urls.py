c4bf9e3b22d484e42768fd9fa9036e95f541e32a
Vadim <jewel.vadim@gmail.com>
2023-01-20 14:46:40 +0400

    small fixes

--- apps/website/urls.py
+++ apps/website/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.views.decorators.csrf import ensure_csrf_cookie
 
 from apps.website.views import IndexTemplateView
 
@@ -7,5 +6,5 @@ from apps.website.views import IndexTemplateView
 app_name = 'website'
 
 urlpatterns = [
-    path('', ensure_csrf_cookie(IndexTemplateView.as_view()), name='index'),
+    path('', IndexTemplateView.as_view(), name='index'),
 ]
