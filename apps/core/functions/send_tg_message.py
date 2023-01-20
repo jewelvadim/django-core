a682f3c61b4e5da884e42c70f6d22e78be71625e
Vadim <jewel.vadim@gmail.com>
2023-01-20 20:22:51 +0400

    fix func

--- apps/core/functions/send_tg_message.py
+++ apps/core/functions/send_tg_message.py
@@ -7,7 +7,10 @@ from requests.exceptions import ConnectionError, ConnectTimeout
 
 @on_exception(wait_gen=expo, exception=(ConnectionError, ConnectTimeout), max_tries=10)
 def send_tg_message(title: str, message: str) -> None:
-    text = f'{title}\n\n{message}'
+    params = {
+        'chat_id': settings.TG_CHAT,
+        'text': f'{title}\n\n{message}',
+    }
 
     if settings.TG_BOT_TOKEN and settings.TG_CHAT:
-        get(f'https://api.telegram.org/bot{settings.TG_BOT_TOKEN}/sendMessage?chat_id={settings.TG_CHAT}&text={text}')
+        get(url=f'https://api.telegram.org/bot{settings.TG_BOT_TOKEN}/sendMessage', params=params)
